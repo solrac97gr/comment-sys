969e55cfb2c1aeb21c555d2a06e676610c83f44a
Carlos <carlosgarcia_97gr@hotmail.com>
2020-03-11 12:37:29 -0500

    add function for get comments

--- controllers/comment.go
+++ controllers/comment.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/solrac97gr/comments/commons"
 	"github.com/solrac97gr/comments/configuration"
@@ -39,9 +41,69 @@ func CommentCreate(w http.ResponseWriter, r *http.Request) {
 	commons.DisplayMessage(w, m)
 }
 
-// GetComments Return all comments of the db
-func GetComments(w http.ResponseWriter, r *http.Request) {
+// CommentGetAll Return all comments of the db
+func CommentGetAll(w http.ResponseWriter, r *http.Request) {
 	comments := []models.Comment{}
 	m := models.Message{}
+	user := models.User{}
+	//vote := models.Vote{}
 
+	r.Context().Value(&user)
+	vars := r.URL.Query()
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	QueryComment := db.Where("parent_id = 0")
+	if order, ok := vars["order"]; ok {
+		if order[0] == "votes" {
+			QueryComment = QueryComment.Order("votes desc, created_at desc")
+		}
+	} else {
+		if idlimit, ok := vars["idlimit"]; ok {
+			registerByPage := 30
+			offset, err := strconv.Atoi(idlimit[0])
+			if err != nil {
+				log.Println("Error:", err)
+			}
+			QueryComment = QueryComment.Where("id BETWEEN ? AND ?", offset-registerByPage, offset)
+		}
+		QueryComment = QueryComment.Order("id desc")
+	}
+	QueryComment.Find(&comments)
+	for i := range comments {
+		db.Model(&comments[i]).Related(&comments[i].User)
+		comments[i].User[0].Password = ""
+		comments[i].Children = commentGetChildren(comments[i].ID)
+	}
+
+	j, err := json.Marshal(comments)
+	if err != nil {
+		m.Code = http.StatusInternalServerError
+		m.Message = "Cant convert the query to json"
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	if len(comments) > 0 {
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m.Code = http.StatusNoContent
+		m.Message = "No content for the query"
+		commons.DisplayMessage(w, m)
+	}
+
+}
+
+func commentGetChildren(id uint) (children []models.Comment) {
+	db := configuration.GetConnection()
+	defer db.Close()
+	db.Where("parent_id=?", id).Find(&children)
+	for i := range children {
+		db.Model(&children[i]).Related(&children[i].User)
+		children[i].User[0].Password = ""
+	}
+	return children
 }
